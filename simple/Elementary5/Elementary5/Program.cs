0ab22cdf855eab05f8b88c18496785d2e003caba
Zach Sadler <zaxcoding@gmail.com>
2014-02-25 13:56:00 -0500

    Two more elementary

--- simple/Elementary5/Elementary5/Program.cs
+++ simple/Elementary5/Elementary5/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Modify the previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
 
-
 using System;
 
 namespace Elementary5
@@ -9,7 +8,27 @@ namespace Elementary5
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			int TheirNum = -1;
+			int sum = 3;
+			while (TheirNum <= 1) {
+				Console.WriteLine ("Hello, I'd like an integer > 3 please.");
+				try {
+					TheirNum = Convert.ToInt32 (Console.ReadLine ());
+				} catch (FormatException ex) {
+					Console.WriteLine ("T_T I said integer");
+				}
+				catch (Exception e) {
+					Console.WriteLine ("ty steve");
+				}
+			}
+			Console.Write ("\n\n3");
+			for (int i = 5; i <= TheirNum; i++) {
+				if (i % 3 == 0 || i % 5 == 0) {
+					Console.Write (" + " + i);
+					sum += i;
+				}
+			}
+			Console.WriteLine (" = " + sum);
 		}
 	}
 }
---
+++ simple/Elementary6/Elementary6/Program.cs
@@ -0,0 +1,34 @@
+﻿// Write a program that asks the user for a number n and gives him
+// the possibility to choose between computing the sum and computing the product of 1,…,n.
+
+
+using System;
+
+namespace Elementary6
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hey man, give me that sweet sweet integer > 1.");
+			Console.WriteLine ("If you choose to disobey me, I'll crash without remorse.");
+			int n = Convert.ToInt32 (Console.ReadLine ()); 
+
+			Console.WriteLine ("Okay.. seems you like actually gave me an int.. now choose" +
+			" if you want to see the sum or the product by saying 'sum' or 'product'.");
+			string ans = Console.ReadLine ();
+
+			if (ans.Equals ("sum")) {
+				Console.WriteLine ("The total sum is " + ((n + 1) * (n) / 2));
+			} else if (ans.Equals ("product")) {
+				uint product = 1;
+				for (uint i = 1; i < n; i++) {
+					product *= i; 
+				}
+				Console.WriteLine ("The total product is " + product);
+			} else { 
+				Console.WriteLine ("You son of a bitch. I'm out.");
+			}
+		}
+	}
+}
