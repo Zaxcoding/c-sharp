f73bbd19e59e459ca047b522e2fa035941e7e6b4
Zach Sadler <zaxcoding@gmail.com>
2014-02-25 16:17:50 -0500

    Two more, including sieve of Eratosthenes

---
+++ simple/Elementary7/Elementary7/Program.cs
@@ -0,0 +1,20 @@
+﻿// Write a program that prints a multiplication table for numbers up to 12.
+
+using System;
+
+namespace Elementary7
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			for (int i = 1; i <= 12; i++) {
+				for (int j = 1; j <= 12; j++) {
+					string ToWrite = string.Format ("\t{0}".PadLeft (8), i * j);
+					Console.Write (ToWrite);
+				}
+				Console.WriteLine ();
+			}
+		}
+	}
+}
---
+++ simple/Elementary8/Elementary8/Program.cs
@@ -0,0 +1,59 @@
+﻿// Write a program that prints all prime numbers. 
+// (Note: if your programming language does not support arbitrary size numbers, 
+// printing all primes up to the largest number you can represent is fine too.)
+
+using System;
+
+namespace Elementary8
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int MAX_PRIME = 0;
+
+			if (args.Length != 1) {
+				Console.WriteLine ("Hey man, give me an integer saying the max number you want to consider for a prime.");
+				return;
+			}
+
+			try {
+				MAX_PRIME = Convert.ToInt32(args[0]);
+			} catch (Exception ex) {
+				Console.WriteLine ("Dude, that's not even an int. Get out.");
+				return;
+			}
+
+			if (MAX_PRIME < 0) {
+				Console.WriteLine ("Really.. this is not even positive. I hate you. I'm done.");
+				return;
+			}
+
+
+			Console.WriteLine ("So I'm gonna print all the primes less than " + MAX_PRIME);
+
+			DateTime start = DateTime.Now;
+
+			int[] BigList = new int[MAX_PRIME];
+			for (int i = 2; i < MAX_PRIME; i++) {
+				if (BigList [i] == 0) {
+					for (int j = 2; i * j < MAX_PRIME; j++) {
+						BigList [i * j]++;	
+					}
+				}
+			}
+
+			DateTime finish = DateTime.Now;
+			TimeSpan diff = finish.Subtract (start);
+
+			for (int i = 2; i < MAX_PRIME; i++) {
+				if (BigList [i] == 0) {
+					Console.Write (i + ", ");
+				}
+			}
+
+			Console.WriteLine ("\nBtw, it took " + diff.ToString () + " to compute those primes.");
+
+		}
+	}
+}
