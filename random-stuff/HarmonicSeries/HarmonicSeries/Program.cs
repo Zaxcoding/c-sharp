5b4d4f52e6e5b9de227ed50e79e82ef53a488fdf
Zach Sadler <zaxcoding@gmail.com>
2014-02-26 22:21:01 -0500

    Added a simple program to demonstrate harmonic series

---
+++ random-stuff/HarmonicSeries/HarmonicSeries/Program.cs
@@ -0,0 +1,27 @@
+﻿// program to demonstrate how slowly the harmonic series 1/n diverges
+
+using System;
+
+namespace HarmonicSeries
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			double sum = 0;
+			double one = 1;
+			int temp = 1;
+
+
+			for (double i = 1; ; i++) {
+				sum += one / i;
+				if ((int)sum == temp) {
+					Console.WriteLine ("It took n = " + i + " to get to " + temp);
+					temp++;
+				}
+			
+			}
+
+		}
+	}
+}
